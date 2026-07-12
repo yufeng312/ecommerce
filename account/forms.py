2081f31056716578327c92a2dc4905977913721d
yufeng312 <b44279467@gmail.com>
2026-07-12 22:57:00 +0800

    feat: 新增忘记密码功能

--- account/forms.py
+++ account/forms.py
@@ -1,15 +1,17 @@
 import re
 
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm, SetPasswordForm)
 
 from .models import User
 
 
 class UserLoginForm(AuthenticationForm):
+
     username = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': '用户名', 'id': 'login-username'}
     ))
+
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={'placeholder': '密码', 'id': 'login-password'}
     ))
@@ -89,6 +91,34 @@ class RegistrationForm(forms.ModelForm):
         if password and password2 and password != password2:
             raise forms.ValidationError('两次输入的密码不一致')
         return password2
+
+
+class PwdResetForm(PasswordResetForm):
+    email = forms.EmailField(label='电子邮箱', max_length=200, widget=forms.TextInput(
+        attrs={'placeholder': '请输入电子邮箱..'}
+    ))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email)
+        if not user:
+            raise ValueError('无效的电子邮箱地址')
+        return email
+    
+
+class PwdResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label='新密码', widget=forms.PasswordInput
+    )
+    new_password2 = forms.CharField(
+        label='确认密码', widget=forms.PasswordInput
+    )
+
+    def clean_new_password1(self):
+        new_password1 = self.cleaned_data.get('new_password1')
+        if new_password1 and self.user.check_password(new_password1):
+            raise forms.ValidationError('新密码不能与旧密码相同')
+        return new_password1
 
 
 class UserEditForm(forms.ModelForm):
--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
-from .forms import UserLoginForm
+from .forms import (UserLoginForm, PwdResetForm, PwdResetConfirmForm)
 from . import views
 
 app_name = 'account'
@@ -13,6 +13,15 @@ urlpatterns = [
                                                 form_class=UserLoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
     path('activate/<slug:uid>/<slug:token>/', views.account_activate, name='activate'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='account/user/password_reset_form.html',
+                                                                success_url=reverse_lazy('account:password_reset_done'),
+                                                                email_template_name='account/user/password_reset_email.html',
+                                                                form_class=PwdResetForm), name='password_reset'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/user/password_reset_confirm.html',
+                                                                                                success_url=reverse_lazy('account:password_reset_complate'),
+                                                                                                form_class=PwdResetConfirmForm), name='password_reset_confirm'),                                                            
+    path('password_reset_done/', TemplateView.as_view(template_name='account/user/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_complate/', TemplateView.as_view(template_name='account/user/password_reset_complate.html'), name='password_reset_complate'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_details, name='edit_details'),
     path('profile/delete_user/', views.delete_user, name='delete_user'),
